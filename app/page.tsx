9f6c51d9e23c8b66449077988d325e906a252176
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-07 16:39:55 +0300

    wip:hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
-      <h1>Кентавър</h1>
+    <main className="w-full min-h-screen flex flex-col lg:flex-col">
+      <div
+        style={{ backgroundImage: `url('hero1.jpg')` }}
+        className="absolute left-0 top-0 z-0 h-[70%] lg:h-full w-full lg:w-1/2 bg-cover bg-center flex justify-center items-center"
+      >
+        <div className="flex flex-col w-full p-6 text-white uppercase text-[13vw] leading-[13vw] lg:text-[6vw] lg:leading-[6vw] font-black">
+          <h1>Таекуондо</h1>
+          <h1>клуб</h1>
+          <h1 className="text-red-800">Кентавър</h1>
+        </div>
+      </div>
+
+      <div
+        style={{ backgroundImage: `url('hero2.jpg')` }}
+        className="absolute right-0 top-[70%] lg:top-0 z-0 h-[70%] lg:h-full w-full lg:w-1/2 bg-cover bg-center"
+      ></div>
     </main>
   );
 }
