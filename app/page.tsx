4c0a316f52855b2ef16e3bfee53daecccfb237b4
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-08 16:32:49 +0300

    add home grid to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero";
+import HomeGrid from "@/components/homeGrid";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen flex flex-col lg:flex-col">
+    <main className="w-full min-h-screen flex flex-col overflow-x-hidden">
       <Hero />
+      <HomeGrid />
     </main>
   );
 }
