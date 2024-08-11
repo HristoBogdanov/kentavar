a06a92f441123269073dc92269c0a7d600711ded
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-11 14:45:33 +0300

    fix hero spacing

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
         style={{ backgroundImage: `url('hero1.jpg')` }}
         className="z-0 h-[70vh] lg:h-[100vh] w-full bg-cover bg-center flex justify-center items-center"
       >
-        <div className="flex flex-col w-full p-6 text-white uppercase text-[13vw] leading-[13vw] lg:text-[6vw] lg:leading-[6vw] font-black">
+        <div className="flex flex-col w-full p-6 text-white uppercase text-[13vw] leading-[13vw] lg:text-[6vw] md:ml-20 gap-3 lg:leading-[6vw] font-black">
           <h1>Таекуондо</h1>
           <h1>клуб</h1>
           <h1 className="text-red-800">Кентавър</h1>
