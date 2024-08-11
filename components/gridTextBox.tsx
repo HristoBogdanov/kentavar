7f72e3a378e01b2c39a6436d68ea5fb46ba3c0ca
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-11 14:51:39 +0300

    add training programme

--- components/gridTextBox.tsx
+++ components/gridTextBox.tsx
@@ -1,26 +1,79 @@
 import Link from "next/link";
+import { FaCalendarCheck } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { IoIosPricetag } from "react-icons/io";
+import { IoPerson } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
 
 interface Props {
   title: string;
-  content: string;
-  buttonText: string;
-  buttonLink: string;
+  timetable: string;
+  time: string;
+  price: string;
+  coach: string;
+  location: string;
+  locationLink: string;
+  color: string;
 }
 
 export default function GridTextBox({
   title,
-  content,
-  buttonText,
-  buttonLink,
+  timetable,
+  time,
+  price,
+  coach,
+  location,
+  locationLink,
+  color,
 }: Props) {
   return (
-    <div className="h-[500px] w-full bg-red-800 p-6 text-white flex items-end">
-      <div className="flex flex-col h-1/2 w-full justify-center gap-6">
-        <h2 className="uppercase font-bold text-3xl">{title}</h2>
-        <p className="text-xl">{content}</p>
-        <Link href={buttonLink} className="text-lg font-black uppercase">
-          {buttonText}
-        </Link>
+    <div
+      style={{ backgroundColor: color }}
+      className="h-[500px] w-full p-6 text-white flex items-center"
+    >
+      <div className="flex flex-col w-full justify-start gap-2">
+        <h2 className="uppercase font-bold pb-6 text-2xl lg:text-3xl">
+          {title}
+        </h2>
+        <div className="flex text-xl items-center gap-3">
+          <FaCalendarCheck />
+          <p>{timetable}</p>
+        </div>
+        <div className="flex text-xl items-center gap-3">
+          <FaClock />
+          <p>{time}</p>
+        </div>
+        <div className="flex text-xl items-center gap-3">
+          <IoIosPricetag />
+          <p>{price}</p>
+        </div>
+        <div className="flex text-xl items-center gap-3">
+          <IoPerson />
+          <p>{coach}</p>
+        </div>
+        <div className="relative flex text-xl items-center gap-3">
+          <Link
+            className="hover:underline transition-all duration-500"
+            href={locationLink}
+            target="__blank"
+          >
+            <MdPlace />
+          </Link>
+          <Link
+            className="hover:underline transition-all duration-500"
+            href={locationLink}
+            target="__blank"
+          >
+            {location}
+          </Link>
+          <Link
+            className="absolute -bottom-5 left-7 hover:underline text-sm transition-all duration-500"
+            href={locationLink}
+            target="__blank"
+          >
+            (виж на карта)
+          </Link>
+        </div>
       </div>
     </div>
   );
--- components/homeGrid.tsx
+++ components/homeGrid.tsx
@@ -1,27 +1,64 @@
+import { kazanlakHallMap, ovoshtnikHallMap } from "@/utils/mapsLinks";
 import GridImage from "./gridImage";
 import GridTextBox from "./gridTextBox";
+import { GiBlackBelt } from "react-icons/gi";
 
 export default function HomeGrid() {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-      <GridImage imageUrl="hero1.jpg" />
-      <GridImage imageUrl="hero1.jpg" />
-      <GridTextBox
-        title="BOXING(ALL LEVELS)"
-        content="Nullam id dolor id nibh ultricies vehicula ut id elit sem lacinia quam venenatis vestibulum.Maecenas sed diameget risus varius blandit"
-        buttonText="view more"
-        buttonLink="/contacts"
-      />
-      <GridImage imageUrl="hero1.jpg" />
-      <GridTextBox
-        title="BOXING(ALL LEVELS)"
-        content="Nullam id dolor id nibh ultricies vehicula ut id elit sem lacinia quam venenatis vestibulum.Maecenas sed diameget risus varius blandit"
-        buttonText="view more"
-        buttonLink="/contacts"
-      />
-      <GridImage imageUrl="hero1.jpg" />
-      <GridImage imageUrl="hero1.jpg" />
-      <GridImage imageUrl="hero1.jpg" />
+    <div className="w-full flex flex-col bg-black justify-center items-center pt-14 gap-14">
+      <div className="flex justify-center items-center gap-6 px-6">
+        <GiBlackBelt className="text-white text-4xl lg:text-6xl" />
+        <h1 className="text-4xl lg:text-6xl uppercase text-white text-center w-fit font-black">
+          нашите <span className="text-red-800">тренировки</span>
+        </h1>
+        <GiBlackBelt className="text-white text-4xl lg:text-6xl" />
+      </div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <GridTextBox
+          title="Таекуондо - начинаещи"
+          timetable="понделник, сряда и петък"
+          time="17:00 - 18:00"
+          price="60 лв. / месец"
+          coach="Николай Богданов"
+          location='Казанлък, ОДК "Св. Иван Рилски"'
+          locationLink={kazanlakHallMap}
+          color="#991B1B"
+        />
+        <GridImage imageUrl="hero1.jpg" />
+        <GridTextBox
+          title="Таекуондо - напреднали"
+          timetable="понделник, сряда и петък"
+          time="17:00 - 18:00"
+          price="60 лв. / месец"
+          coach="Николай Богданов"
+          location='Казанлък, ОДК "Св. Иван Рилски"'
+          locationLink={kazanlakHallMap}
+          color="#991B1B"
+        />
+        <GridImage imageUrl="hero1.jpg" />
+        <GridTextBox
+          title="Таекуондо - начинаещи"
+          timetable="понделник, сряда и петък"
+          time="17:30 - 18:30"
+          price="50 лв. / месец"
+          coach="Любомир Богданов"
+          location="Овощник, Жельо Манолов 58"
+          locationLink={ovoshtnikHallMap}
+          color="#101010"
+        />
+        <GridImage imageUrl="hero1.jpg" />
+        <GridTextBox
+          title="Тае-бо"
+          timetable="понделник, сряда и петък"
+          time="19:00 - 20:00"
+          price="50 лв. / месец"
+          coach="Любомир Богданов"
+          location="Овощник, Жельо Манолов 58"
+          locationLink={ovoshtnikHallMap}
+          color="#101010"
+        />
+        <GridImage imageUrl="hero1.jpg" />
+      </div>
     </div>
   );
 }
