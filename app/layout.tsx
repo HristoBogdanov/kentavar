69484afe6939c76e3514151bbb6722619c5749f8
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-17 21:05:56 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,29 @@ const oswald = Oswald({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ТК Кентавър",
-  description: "ТК Кентавър",
+  icons: {
+    icon: "/logo.jpeg",
+  },
+  description:
+    "ТК Кентавър - тренировки по олимпийско таекуондо и тае-бо в град Казанлък и село Овощник с треньори Николай, Христо и Любомир Богданови и Любомир Събчев.",
+  keywords: "таекуондо, тренировки, спорт, тае-бо, Казанлък, Овощник",
+  robots: "index, follow",
+  openGraph: {
+    title: "ТК Кентавър",
+    description:
+      "ТК Кентавър - тренировки по олимпийско таекуондо и тае-бо в град Казанлък и село Овощник с треньори Николай, Христо и Любомир Богданови и Любомир Събчев.",
+    url: "https://tk-centaur.vercel.app/",
+    siteName: "ТК Кентавър",
+    images: [
+      {
+        url: "/group.jpeg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "bg-BG",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
