680e0f2649aa21fc88a41591ffa18c43250688df
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-20 13:29:16 +0300

    useInView custom hook

---
+++ hooks/useInView.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useInView(threshold: number = 0.5) {
+  const [inView, setInView] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setInView(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [threshold]);
+
+  return { ref, inView };
+}
