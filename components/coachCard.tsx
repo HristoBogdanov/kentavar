9568255c4df9c9dff6a4c1b66f4987eef24087d0
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-08-28 10:40:01 +0300

    add coach section

---
+++ components/coachCard.tsx
@@ -0,0 +1,34 @@
+import { GiBlackBelt } from "react-icons/gi";
+import RevealDivBottomToTopDelay from "./animations/revealFromTopDelay";
+
+interface Props {
+  name: string;
+  desc: string[];
+  imagePath: string;
+  delay: number;
+}
+
+export default function coachCard({ name, desc, imagePath, delay }: Props) {
+  return (
+    <RevealDivBottomToTopDelay
+      delay={delay}
+      className="w-full flex flex-col justify-center items-center border-t-8 border-b-8 border-red-800 bg-softBlack"
+    >
+      <div
+        className="w-full aspect-[4/5] bg-cover bg-center"
+        style={{ backgroundImage: `url('${imagePath}')` }}
+      ></div>
+      <div className="flex flex-col w-full max-sm:h-fit max-xl:350px h-[500px] max-xl:justify-center 2xl:h-[350px] 2xl:justify-center p-6 gap-10 items-center text-white">
+        <h3 className="text-3xl font-black">{name}</h3>
+        <ul className="text-xl flex flex-col gap-3">
+          {desc.map((fact, i) => (
+            <li className="flex items-center gap-3 w-full" key={i}>
+              <GiBlackBelt className="min-w-6 min-h-6" />
+              <p>{fact}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </RevealDivBottomToTopDelay>
+  );
+}
---
+++ components/coachesSection.tsx
@@ -0,0 +1,63 @@
+import Heading from "./heading";
+import CoachCard from "./coachCard";
+
+const nikiDesc = [
+  "Треньор с над 20 години опит",
+  'Двукратен носител на наградата "Треньор на годината"',
+  "Бивш национален състезател",
+  "Медалист от Балканско първенство",
+];
+
+const luboBDesc = [
+  "Двукратен вицеевропейски шампион",
+  "5-то място на Световно първенство",
+  "5-то място на Европейски олимпийски игри",
+  'Двукратен носител на наградата "Спортист на годината"',
+];
+
+const icoDesc = [
+  "Двукратен европейски шампион",
+  "Бронзов медалист от Европейско първенство",
+  "Балкански шампион",
+  'Носител на награда "Най-добър таекуондист на България за 2019г."',
+];
+
+const luboSDesc = [
+  "Треньор с богат опит в дисциплини спраинг и пумсе",
+  "Дългогодишен състезател с множество успехи",
+  "Участник на елитни международни турнири",
+];
+
+export default function coachesSection() {
+  return (
+    <div className="w-full flex flex-col bg-black justify-center items-center gap-14">
+      <Heading firstTitle="нашите" secondTitle="треньори" />
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 px-6 lg:px-10">
+        <CoachCard
+          name="Николай Богданов"
+          desc={nikiDesc}
+          imagePath="lubo3.jpg"
+          delay={0.25}
+        ></CoachCard>
+        <CoachCard
+          name="Любомир Богданов"
+          desc={luboBDesc}
+          imagePath="lubo3.jpg"
+          delay={0.5}
+        ></CoachCard>
+        <CoachCard
+          name="Христо Богданов"
+          desc={icoDesc}
+          imagePath="lubo3.jpg"
+          delay={0.75}
+        ></CoachCard>
+        <CoachCard
+          name="Любомир Събчев"
+          desc={luboSDesc}
+          imagePath="lubo3.jpg"
+          delay={1}
+        ></CoachCard>
+      </div>
+    </div>
+  );
+}
