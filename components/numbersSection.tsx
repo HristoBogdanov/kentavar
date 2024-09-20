6ccc77b3437bf2751da3aa8daf3e3041fa6e3298
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-09-20 09:51:39 +0300

    fix numbers section responsive

--- components/numbersSection.tsx
+++ components/numbersSection.tsx
@@ -4,14 +4,14 @@ import StatisticBox from "./statisticBox";
 
 export default function NumbersSection() {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2">
+    <div className="w-full grid grid-cols-1 xl:grid-cols-2">
       <div className="flex flex-col justify-center items-center gap-10 py-20 lg:py-40 bg-black border-b-8 border-t-8 border-red-800 text-white">
         <FadeIn>
           <h1 className="uppercase text-5xl text-center font-black px-6">
             Нашата <span className="text-red-800">история</span> в цифри
           </h1>
         </FadeIn>
-        <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-10 px-6">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10 px-6">
           <StatisticBox number={50} text="състезатели" />
           <StatisticBox number={30} text="отборни награди" />
           <StatisticBox number={4} text="европейски медалисти" />
