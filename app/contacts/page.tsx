6fd049c36bb0e22b6d457c921c49980811c18650
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-09-24 14:23:48 +0300

    fix metadata typo

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,6 +1,10 @@
 import ContactsSection from "@/components/contactsSection";
 import TrainingCentresSction from "@/components/trainingCentresSction";
 
+export const metadata = {
+  title: "Контакти",
+};
+
 export default function page() {
   return (
     <main className="w-full min-h-screen flex flex-col bg-black gap-10 lg:gap-20">
